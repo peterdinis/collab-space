f3d239846d5f607f5bebf118a8d64ed5dc7adf68
Peter Dinis <peterdinis0@pobox.sk>
2024-08-03 21:45:11 +0200

    feat: Start to updating landing page WIP

--- app/_components/home/HomeWrapper.tsx
+++ app/_components/home/HomeWrapper.tsx
@@ -1,49 +1,84 @@
-import { File, ArrowRight } from "lucide-react";
 import { FC } from "react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Footer } from "../shared/Footer";
+import mainImage from "../../../public/img/main.webp"
+import Image from "next/image";
 
 const HomeWrapper: FC = () => {
   return (
-    <div className="flex min-h-full flex-col dark:bg-[#1F1F1F]">
-      <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
-        <div className="max-4xl space-y-4">
-          <main className="h-full pt-40">
-            <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
-              Your
-              <File size={38} className="m-2 inline-flex" />
-              <span className="underline decoration-from-font underline-offset-4">
-                wiki
-              </span>
-              ,
-              <File size={38} className="m-2 inline-flex" />
-              <span className="underline decoration-from-font underline-offset-4">
-                docs
-              </span>
-              ,
-              <br /> &amp;
-              <File size={38} className="m-2 inline-flex" />
-              <span className="underline decoration-from-font underline-offset-4">
-                projects
-              </span>
-              . Together.
-            </h1>
-            <h3 className="text-base mt-10 font-medium sm:text-xl md:text-2xl">
-              Collab Orbit is the connected workspace where <br />
-              better, faster work happens.
-            </h3>
-
-            <Button asChild className="mt-5">
-              <Link href="/login">
-                Try now
-                <ArrowRight className="ml-2 h-4 w-4" />
-              </Link>
-            </Button>
-          </main>
+    <div className="pt-32 md:py-12 mt-20 xl:container m-auto px-6 md:px-12">
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 my-auto w-96 h-32 rotate-45 bg-gradient-to-r from-primaryLight to-secondaryLight blur-3xl opacity-50 dark:opacity-20"
+      ></div>
+      <div className="relative lg:flex lg:items-center lg:gap-12">
+        <div className="text-center lg:text-left md:mt-12 lg:mt-0 sm:w-10/12 md:w-2/3 sm:mx-auto lg:mr-auto lg:w-6/12">
+          <h1 className="text-gray-900 font-bold text-4xl md:text-6xl lg:text-5xl xl:text-6xl dark:text-white">
+            Build in your way but with our experts{" "}
+            <span className="text-primary dark:text-primaryLight">
+              Support.
+            </span>
+          </h1>
+          <p className="mt-8 text-gray-600 dark:text-gray-300">
+            Odio incidunt nam itaque sed eius modi error totam sit illum.
+            Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis
+            beatae ipsum soluta!
+          </p>
+          <div>
+            <form action="" className="w-full mt-12">
+              <div className="relative flex items-center px-2 p-1 rounded-full bg-white dark:bg-gray-900 border dark:border-gray-700 border-primary/10 shadow-md md:p-2 lg:pr-3">
+                <div className="pl-6 py-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 m-auto fill-blue-900/60 dark:fill-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  </svg>
+                </div>
+                <input
+                  autoComplete="email"
+                  placeholder="Your mail address"
+                  className="w-full p-4 rounded-full placeholder-gray-600 dark:placeholder-white bg-transparent"
+                  type="email"
+                />
+                <div className="md:pr-1.5 lg:pr-0">
+                  <button
+                    type="button"
+                    title="Start buying"
+                    className="relative h-12 w-20 sm:w-auto ml-auto sm:px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary dark:before:bg-primaryLight before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
+                  >
+                    <span className="relative hidden w-max text-white dark:text-gray-900 font-semibold md:block">
+                      Get Started
+                    </span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="relative h-6 w-6 mx-auto text-white dark:text-gray-900 md:hidden"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="overflow-hidden w-full lg:w-7/12 lg:-mr-16">
+          <Image
+            src={mainImage}
+            className="w-full"
+            alt="project illustration"
+          />
         </div>
       </div>
-      <Footer />
     </div>
   );
 };
