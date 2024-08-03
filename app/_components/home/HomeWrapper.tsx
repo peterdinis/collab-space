a0a5a27fa218e4a2bbe57c33a9f707c51138c8b4
Peter Dinis <peterdinis0@pobox.sk>
2024-08-03 15:35:30 +0200

    FOO

--- app/_components/home/HomeWrapper.tsx
+++ app/_components/home/HomeWrapper.tsx
@@ -2,9 +2,15 @@ import { FC } from "react";
 
 const HomeWrapper: FC = () => {
     return (
-        <>
-        aaaaa
-        </>
+        <div className="flex min-h-full flex-col dark:bg-[#1F1F1F]">
+             <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
+                <div className="max-4xl space-y-4">
+                    <main className="h-full pt-40">
+                        COLLAP SPACE
+                    </main>
+                </div>
+             </div>
+        </div>
     )
 }
 
