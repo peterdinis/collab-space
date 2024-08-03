977c78fe98d7e38e2847836ce3cd648bea404ec6
Peter Dinis <peterdinis0@pobox.sk>
2024-08-03 22:18:20 +0200

    feat: Design landing page WIP

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import { NextPage } from "next";
 import HomeWrapper from "./_components/home/HomeWrapper";
 import HomeServices from "./_components/home/HomeServices";
 import { Footer } from "./_components/shared/Footer";
+import AnimationWrapper from "./_components/shared/AnimationWrapper";
 
 const Homepage: NextPage = () => {
   return (
-    <>
+    <AnimationWrapper>
       <HomeWrapper />
       <HomeServices />
       <Footer />
-    </>
+    </AnimationWrapper>
   )
 }
 
