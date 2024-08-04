d63d9e4237c66c145be4d745e87ab5cab1dbc0e6
Peter Dinis <peterdinis0@pobox.sk>
2024-08-04 13:17:06 +0200

    feat: Adding clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./_components/shared/Navigation";
 import ThemeProvider from "./_components/shared/providers/ThemeProvider";
+import ClerkAppProvider from "./_components/shared/providers/ClerkAppProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="dark">
-          <Navigation />
-          {children}
+          <ClerkAppProvider>
+            <Navigation />
+            {children}
+          </ClerkAppProvider>
         </ThemeProvider>
       </body>
     </html>
