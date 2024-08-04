b874e97f3ca48ef66e5a6639b0e8ad0b0f7d9579
Peter Dinis <peterdinis0@pobox.sk>
2024-08-04 21:35:43 +0200

    FOO

--- app/_components/shared/sidebar/Sidebar.tsx
+++ app/_components/shared/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 
 const Sidebar = () => {
   return (
-    <div className="hidden border-r bg-muted/40 md:block">
+    <div className="hidden border-r pt-20 bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
