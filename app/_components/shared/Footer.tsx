a9e6ea91c4535528ee67917816080632bdf34ec9
Peter Dinis <peterdinis0@pobox.sk>
2024-08-05 10:41:48 +0200

    FOO

--- app/_components/shared/Footer.tsx
+++ app/_components/shared/Footer.tsx
@@ -6,10 +6,10 @@ import { Orbit } from 'lucide-react';
 export const Footer = () => {
     return (
         <div className='z-50 flex w-full items-center bg-background p-6 text-2xl font-bold dark:bg-[#1F1F1F]'>
-            Collab <Orbit className='ml-2 size-8' />
+            Collab <Orbit className='ml-2 size-8' /> &nbsp;&copy;2024
             <div className='flex w-full items-center justify-between gap-x-2 text-muted-foreground md:ml-auto md:justify-end'>
                 <Button variant='ghost' size='lg'>
-                    Privacy Policy
+                    Services
                 </Button>
                 <Button variant='ghost' size='lg'>
                     Terms & Conditions
