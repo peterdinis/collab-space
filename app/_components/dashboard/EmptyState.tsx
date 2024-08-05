97a2a538a42273f715397cecd94e625dec8eb516
Peter Dinis <peterdinis0@pobox.sk>
2024-08-05 16:26:02 +0200

    FOO

--- app/_components/dashboard/EmptyState.tsx
+++ app/_components/dashboard/EmptyState.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { FC } from "react"
 
-const EmptyState = () => {
+const EmptyState: FC = () => {
   return (
     <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1">
       <div className="flex flex-col items-center gap-1 text-center">
-        <h3 className="text-2xl font-bold tracking-tight">You have no products</h3>
-        <p className="text-sm text-muted-foreground">You can start selling as soon as you add a product.</p>
-        <Button className="mt-4">Add Product</Button>
+        <h3 className="text-2xl font-bold tracking-tight">Hey Welcome here</h3>
+        <p className="text-xl mt-5 text-muted-foreground">Create new workspace or team to get started.</p>
       </div>
     </div>
   )
