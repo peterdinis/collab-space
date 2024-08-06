7aeea53514b83a5290475772eb63dfafc00b6484
Peter Dinis <peterdinis0@pobox.sk>
2024-08-06 17:07:14 +0200

    feat: Working on auth WIP

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,8 @@
 import { NextPage } from "next";
+import LoginForm from "../_components/auth/LoginForm";
 
 const LoginPage: NextPage = () => {
-    return (
-        <>
-        aaaaa
-        </>
-    )
+    return <LoginForm />
 }
 
 export default LoginPage;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,8 @@
 import { NextPage } from "next";
+import RegisterForm from "../_components/auth/RegisterForm";
 
 const RegisterPage: NextPage = () => {
-    return (
-        <>
-        aaaaa
-        </>
-    )
+    return <RegisterForm />
 }
 
 export default RegisterPage;
