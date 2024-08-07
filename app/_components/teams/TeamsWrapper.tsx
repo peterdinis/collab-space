0ab7e18c478c1e8b56755e048da335f10e97e155
Peter Dinis <peterdinis0@pobox.sk>
2024-08-07 14:36:45 +0200

    FOO

---
+++ app/_components/teams/TeamsWrapper.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import Sidebar from '../shared/sidebar/Sidebar';
+import DashboardHeader from '../dashboard/DashboardHeader';
+
+const TeamsWrapper: FC = () => {
+    return (
+        <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
+            <Sidebar />
+            <div className='flex flex-col'>
+                <DashboardHeader />
+                <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+                    <div className='flex items-center'>
+                        <h1 className='prose-h1: prose ml-2 mt-6 text-lg font-semibold dark:text-white md:text-2xl'>
+                            My Teams
+                        </h1>
+                    </div>
+                    rrrrrr
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default TeamsWrapper;
---
+++ app/teams/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import TeamsWrapper from "../_components/teams/TeamsWrapper";
+
+const TeamsPage: NextPage = () => {
+    return <TeamsWrapper />
+}
+
+export default TeamsPage;
