e72eadd4c860f32e9185c8f6d2426631c0b40df4
Peter Dinis <peterdinis0@pobox.sk>
2024-08-07 16:57:04 +0200

    FOO

--- app/_components/workspaces/WorkspacesWrapper.tsx
+++ app/_components/workspaces/WorkspacesWrapper.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC } from 'react';
+import Sidebar from '../shared/sidebar/Sidebar';
+import DashboardHeader from '../dashboard/DashboardHeader';
+import Header from '../shared/Header';
+import { Input } from '@/components/ui/input';
+import WorkspacesPagination from './WorkspacesPagination';
 
 const WorkspacesWrapper: FC = () => {
     return (
-        <>
-            WRAPPER
-        </>
-    )
-}
+        <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'>
+            <Sidebar />
+            <div className='flex flex-col'>
+                <DashboardHeader />
+                <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+                    <div className='flex items-center'>
+                        <Header text='My Workspaces' />
+                    </div>
+                    <Input placeholder='Search...' />
+                    <div className='ml-4'>
+                        WORKSPACES
+                    </div>
+                    <WorkspacesPagination />
+                </main>
+            </div>
+        </div>
+    );
+};
 
 export default WorkspacesWrapper;
