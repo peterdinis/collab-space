0a1c0f71777c50cea4f687920f5ce9f48712f607
Peter Dinis <peterdinis0@pobox.sk>
2024-08-07 16:40:36 +0200

    feat: Working on workspaces

---
+++ app/_components/workspaces/WorkspacesWrapper.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const WorkspacesWrapper: FC = () => {
+    return (
+        <>
+            WRAPPER
+        </>
+    )
+}
+
+export default WorkspacesWrapper;
---
+++ app/workspaces/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import WorkspacesWrapper from "../_components/workspaces/WorkspacesWrapper";
+
+const WorkspacesPage: NextPage = () => {
+    return <WorkspacesWrapper />
+}
+
+export default WorkspacesPage
