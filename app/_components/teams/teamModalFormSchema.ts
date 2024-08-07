8840f5138b5715aa342adaf44cadbea0f4ed44f1
Peter Dinis <peterdinis0@pobox.sk>
2024-08-07 21:05:58 +0200

    feat: New team logic WIP

--- app/_components/teams/teamModalFormSchema.ts
+++ app/_components/teams/teamModalFormSchema.ts
@@ -4,4 +4,8 @@ export const formSchema = z.object({
     name: z.string().min(2, {
         message: 'name must be at least 2 characters.',
     }),
+
+    description: z.string().min(2, {
+        message: "Description for teams must be at least 2 characters"
+    })
 });
