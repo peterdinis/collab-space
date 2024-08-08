7175ec8eeeac4d3b014fcedfb685ac04b7b64105
Peter Dinis <peterdinis0@pobox.sk>
2024-08-08 18:13:59 +0200

    FOO

--- app/_components/dashboard/EmptyState.tsx
+++ app/_components/dashboard/EmptyState.tsx
@@ -4,7 +4,6 @@ const EmptyState: FC = () => {
     return (
         <div
             className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm'
-            x-chunk='dashboard-02-chunk-1'
         >
             <div className='flex flex-col items-center gap-1 text-center'>
                 <h3 className='text-2xl font-bold tracking-tight'>
--- app/_components/shared/sidebar/Sidebar.tsx
+++ app/_components/shared/sidebar/Sidebar.tsx
@@ -2,14 +2,6 @@
 
 import Link from 'next/link';
 import { FileText, Home, LogOut, Package, Users, Users2 } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
 import { useAuth } from '@/app/_context/AuthContext';
 import { useToast } from '@/components/ui/use-toast';
 import { useRouter } from 'next/navigation';
@@ -87,22 +79,6 @@ const Sidebar = () => {
                             Logout
                         </div>
                     </nav>
-                </div>
-                <div className='mt-auto p-4'>
-                    <Card x-chunk='dashboard-02-chunk-0'>
-                        <CardHeader className='p-2 pt-0 md:p-4'>
-                            <CardTitle>Upgrade to Pro</CardTitle>
-                            <CardDescription>
-                                Unlock all features and get unlimited access to
-                                our support team.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className='p-2 pt-0 md:p-4 md:pt-0'>
-                            <Button size='sm' className='w-full'>
-                                Upgrade
-                            </Button>
-                        </CardContent>
-                    </Card>
                 </div>
             </div>
         </div>
